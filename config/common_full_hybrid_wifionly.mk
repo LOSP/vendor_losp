1a3cec482613166bdf4df9e4e4193209fcc3a150
PeterCxy <xqsx43cxy@126.com>
2013-11-10 12:53:55 +0800

    losp: remove slim bluetooth stuffs

--- config/common_full_hybrid_wifionly.mk
+++ config/common_full_hybrid_wifionly.mk
@@ -2,7 +2,3 @@
 $(call inherit-product, vendor/losp/config/common.mk)
 
 PRODUCT_PACKAGE_OVERLAYS += vendor/losp/overlay/tablet
-
-# BT config
-PRODUCT_COPY_FILES += \
-    system/bluetooth/data/main.nonsmartphone.conf:system/etc/bluetooth/main.conf
--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -1,6 +1,2 @@
 # Inherit common stuff
 $(call inherit-product, vendor/losp/config/common.mk)
-
-# BT config
-PRODUCT_COPY_FILES += \
-    system/bluetooth/data/main.conf:system/etc/bluetooth/main.conf
